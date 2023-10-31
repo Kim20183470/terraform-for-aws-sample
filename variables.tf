091470e846b886f85099e8c262967f4e76af4964
Kim20183470 <114399474+Kim20183470@users.noreply.github.com>
2023-10-31 11:24:56 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  type        = string
+  description = "Please input your [Region]"
+  default     = "ap-northeast-2"
+  validation {
+    condition     = contains(["ap-south-1"], var.aws_region)
+    error_message = "You must use [ap-south-1]"
+  }
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "Please input your [AWS Access Key]"
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "Please input your [AWS Secret Key]"
+  sensitive = true
+}
